9a9de2838f0bc8c0bf7c05add3b88de24cfceb72
Dustin Gogoll <dustin.gogoll@googlemail.com>
2019-07-19 00:15:53 +0200

    SSEManager adjusted

--- eventSystem/SSEManager.js
+++ eventSystem/SSEManager.js
@@ -7,9 +7,16 @@ class SSEManager {
     app.get('/event-stream', this.sse.init)
 
     // A test interval
+    //  TODO: Has to be removed when not needed anymore
     setInterval(() => {
       this.sse.send({test: 'test'}, 'test');
     }, 3000)
+
+    this.sendEvent = this.sendEvent.bind(this);
+  }
+
+  sendEvent(eventName, content) {
+    this.sse.send(content, eventName);
   }
 }
 
